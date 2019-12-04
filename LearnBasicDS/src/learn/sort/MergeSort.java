8ae3d7eb59fda3312f169b5c9eff5ec7da9ca606
Yogesh <Yogesh@Predator>
2019-12-04 16:09:32 +0530

    Hello World

---
+++ LearnBasicDS/src/learn/sort/MergeSort.java
@@ -0,0 +1,14 @@
+package learn.sort;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr = {1,2,3,4,5};
+		
+		Arrays.stream(arr).forEach(System.out::println);
+	}
+
+}
