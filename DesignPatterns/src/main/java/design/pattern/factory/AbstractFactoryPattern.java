05f8bb57de0d22f1c47663665196e4039eebc70a
Yogesh <yogeshs3345@gmail.com>
2022-08-02 22:16:11 +0530

    added link for Abstract Factory Pattern

---
+++ DesignPatterns/src/main/java/design/pattern/factory/AbstractFactoryPattern.java
@@ -0,0 +1,11 @@
+package design.pattern.factory;
+
+public class AbstractFactoryPattern {
+
+    /*
+        This is higher level version of Factory Pattern. Understand how to create and use it,
+        and more importantly when to use it.
+
+        Link: https://www.baeldung.com/java-abstract-factory-pattern
+     */
+}
